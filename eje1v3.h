68f49e73bf3dbcf9085fef3cc4d6d74d6929a973
RosaRGTorres <31104728+RosaRGTorres@users.noreply.github.com>
2020-05-10 16:55:27 -0500

    Update eje1v3.h

--- eje1v3.h
+++ eje1v3.h
@@ -231,233 +231,233 @@ int abrirArchivo(char parametro[]) {
 /* */
 
 
-/* eje1v3.c
- * Muestra archivo en hexadecimal
- * Ejecutar: gcc ejemploSO1.c eje1v3.h -lncurses -o ./ejemploSO1
- * Autores: Rosa González, Jovanny Ulloa, Carolina Cedillo
- */
+// /* eje1v3.c
+//  * Muestra archivo en hexadecimal
+//  * Ejecutar: gcc ejemploSO1.c eje1v3.h -lncurses -o ./ejemploSO1
+//  * Autores: Rosa González, Jovanny Ulloa, Carolina Cedillo
+//  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <ctype.h>
-#include <string.h>
-#include <curses.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <fcntl.h>
+// #include <ctype.h>
+// #include <string.h>
+// #include <curses.h>
+// #include <sys/stat.h>
+// #include <sys/mman.h>
 
-// limpiar pantalla en Windows o linux
-#ifdef _WIN32
-#define CLEAR "cls"
-#else
-#define CLEAR "clear"
-#endif
-#define FILEPATH "eje1v3.c"
+// // limpiar pantalla en Windows o linux
+// #ifdef _WIN32
+// #define CLEAR "cls"
+// #else
+// #define CLEAR "clear"
+// #endif
+// #define FILEPATH "eje1v3.c"
 
-/*Editar*/
-int leeChar() {
-  int chars[5];
-  int ch, i = 0;
-  nodelay(stdscr, TRUE);
-  while ((ch = getch()) == ERR); /* Espera activa */
-  ungetch(ch);
-  while ((ch = getch()) != ERR) {
-    chars[i++] = ch;
-  }
-  /* convierte a numero con todo lo leido */
-  int res = 0;
-  for (int j = 0; j < i; j++) {
-    res <<= 8;
-    res |= chars[j];
-  }
-  return res;
-}
+// /*Editar*/
+// int leeChar() {
+//   int chars[5];
+//   int ch, i = 0;
+//   nodelay(stdscr, TRUE);
+//   while ((ch = getch()) == ERR); /* Espera activa */
+//   ungetch(ch);
+//   while ((ch = getch()) != ERR) {
+//     chars[i++] = ch;
+//   }
+//   /* convierte a numero con todo lo leido */
+//   int res = 0;
+//   for (int j = 0; j < i; j++) {
+//     res <<= 8;
+//     res |= chars[j];
+//   }
+//   return res;
+// }
 
-char * hazLinea(char * base, int dir) {
-  char linea[100]; //la linea es más pequeña
-  int o = 0;
-  //Muestra 16 caracteres por cada linea
-  o += sprintf(linea, "%08x", dir); //offset en hexadecimal
-  for (int i = 0; i < 4; i++) {
-    unsigned char a, b, c, d;
-    a = base[dir + 4 * i + 0];
-    b = base[dir + 4 * i + 1];
-    c = base[dir + 4 * i + 2];
-    d = base[dir + 4 * i + 3];
-    o += sprintf( & linea[o], " %02x %02x %02x %02x", a, b, c, d);
-  }
-  for (int i = 0; i < 16; i++) {
-    if (isprint(base[dir + i])) {
-      o += sprintf( & linea[o], "%c", base[dir + i]);
-    } else {
-      o += sprintf( & linea[o], ".");
-    }
-  }
-  sprintf( & linea[o], "\n");
+// char * hazLinea(char * base, int dir) {
+//   char linea[100]; //la linea es más pequeña
+//   int o = 0;
+//   //Muestra 16 caracteres por cada linea
+//   o += sprintf(linea, "%08x", dir); //offset en hexadecimal
+//   for (int i = 0; i < 4; i++) {
+//     unsigned char a, b, c, d;
+//     a = base[dir + 4 * i + 0];
+//     b = base[dir + 4 * i + 1];
+//     c = base[dir + 4 * i + 2];
+//     d = base[dir + 4 * i + 3];
+//     o += sprintf( & linea[o], " %02x %02x %02x %02x", a, b, c, d);
+//   }
+//   for (int i = 0; i < 16; i++) {
+//     if (isprint(base[dir + i])) {
+//       o += sprintf( & linea[o], "%c", base[dir + i]);
+//     } else {
+//       o += sprintf( & linea[o], ".");
+//     }
+//   }
+//   sprintf( & linea[o], "\n");
 
-  return (strdup(linea));
-}
+//   return (strdup(linea));
+// }
 
-/* Función para abrir los archivos con los modos de apertura indicados posteriormente */
-int abrirArchivo(char parametro[]) {
-  system(CLEAR);
-  initscr();
-  raw();
-  noecho(); //no mostar el caracter leido
+// /* Función para abrir los archivos con los modos de apertura indicados posteriormente */
+// int abrirArchivo(char parametro[]) {
+//   system(CLEAR);
+//   initscr();
+//   raw();
+//   noecho(); //no mostar el caracter leido
 
-  //Abrir archivo
-  printf("%s\n", parametro);
-  int fdl = open(parametro, O_RDONLY);
-  //int fde = open (parametro, O_RDWR);
-  int fde = open(parametro, O_RDWR | O_CREAT, 0600); // Abre con permisos -rw-
+//   //Abrir archivo
+//   printf("%s\n", parametro);
+//   int fdl = open(parametro, O_RDONLY);
+//   //int fde = open (parametro, O_RDWR);
+//   int fde = open(parametro, O_RDWR | O_CREAT, 0600); // Abre con permisos -rw-
 
-  if (fdl == -1) {
-    perror("Error abriendo el archivo");
-    exit(EXIT_FAILURE);
-  }
-  if (fde == -1) {
-    perror("Error abriendo el archivo");
-    exit(EXIT_FAILURE);
-  }
+//   if (fdl == -1) {
+//     perror("Error abriendo el archivo");
+//     exit(EXIT_FAILURE);
+//   }
+//   if (fde == -1) {
+//     perror("Error abriendo el archivo");
+//     exit(EXIT_FAILURE);
+//   }
 
-  struct stat st;
-  fstat(fdl, & st);
-  int fs = st.st_size;
+//   struct stat st;
+//   fstat(fdl, & st);
+//   int fs = st.st_size;
 
-  /*Mapeo el archivo de entrada*/
-  char * mapo = mmap(0, fs, PROT_READ, MAP_SHARED, fdl, 0);
-  if (mapo == MAP_FAILED) {
-    close(fdl);
-    perror("Error mapeando el archivo fdl");
-    exit(EXIT_FAILURE);
-  }
-  /*Mapeo el archivo de salida*/
-  const int SLACK = 512;
+//   /*Mapeo el archivo de entrada*/
+//   char * mapo = mmap(0, fs, PROT_READ, MAP_SHARED, fdl, 0);
+//   if (mapo == MAP_FAILED) {
+//     close(fdl);
+//     perror("Error mapeando el archivo fdl");
+//     exit(EXIT_FAILURE);
+//   }
+//   /*Mapeo el archivo de salida*/
+//   const int SLACK = 512;
   
-  char * map = mmap(0, fs + SLACK, PROT_READ | PROT_WRITE, MAP_SHARED, fde, 0);
-  if (map == MAP_FAILED) {
-    close(fde);
-    perror("Error mapeando el archivo fde");
-    exit(EXIT_FAILURE);
-  }
+//   char * map = mmap(0, fs + SLACK, PROT_READ | PROT_WRITE, MAP_SHARED, fde, 0);
+//   if (map == MAP_FAILED) {
+//     close(fde);
+//     perror("Error mapeando el archivo fde");
+//     exit(EXIT_FAILURE);
+//   }
 
-  /*Copia uno en otro*/
-  memcpy(map, mapo, fs);
+//   /*Copia uno en otro*/
+//   memcpy(map, mapo, fs);
 
-  for (int i = 0; i < 25; i++) {
-    //Haz linea, base y offset
-    char * l = hazLinea(map, i * 16);
-    //mvprintw(i,0,l);
-    move(i, 0);
-    addstr(l);
-  }
-  refresh();
+//   for (int i = 0; i < 25; i++) {
+//     //Haz linea, base y offset
+//     char * l = hazLinea(map, i * 16);
+//     //mvprintw(i,0,l);
+//     move(i, 0);
+//     addstr(l);
+//   }
+//   refresh();
 
-  int ch, ch2;
-  int c = 0, r = 0;
-  int x = 0, y = 0;
-  move(c, 9 + r);
-  long b;
-  //char *p;
-  // char *jovas[2];
+//   int ch, ch2;
+//   int c = 0, r = 0;
+//   int x = 0, y = 0;
+//   move(c, 9 + r);
+//   long b;
+//   //char *p;
+//   // char *jovas[2];
 
-  do {
-    ch = leeChar();
-    switch (ch) {
-    case 0x1b5b41:
-      r = (r > 0) ? r - 1 : 24;
-      break;
-    case 0x1b5b42:
-      r = (r < 24) ? r + 1 : 0;
-      break;
-    case 0x1b5b43:
-      c = (c < 31) ? c + 1 : 0;
-      break;
-    case 0x1b5b44:
-      c = (c > 0) ? c - 1 : 31;
-      break;
-    default:
-      if (c < 16) {
-        char largo[3];
-        char o = tolower(ch);
-        ch2 = leeChar();
-        char m = tolower(ch2);
-        char * fin = "\0";
-        largo[0] = o;
-        largo[1] = m;
-        largo[2] = * fin;
-        /* Validamos los dos caracteres ingresados*/
-        /*int validaChars[2] = {0,0};
-        if (o >= '0' && o >= '9' || (o >= 'a' && o <= 'f')){
-          validaChars[0] =1;
-        }
-        if (o >= '0' && o >= '9' || (o >= 'a' && o <= 'f')){
-          validaChars[1] =1;
-        }
-        if(validaChars[0] == 1 && validaChars[1] == 1){
-          b = strtol(largo, NULL, 16);
-          map[r * 16 + c] = b;
-        }*/
-        b = strtol(largo, NULL, 16);
-         map[r * 16 + c] = b;
-      }
-      if (c > 16) {
-        char n = tolower(ch);
-        if (n >= '0' && n <= '9' || (n >= 'a' && n <= 'f')) {
-          // b = strtol(n, NULL, 16);
-          map[r * 16 + c - 16] = n;
-          // char largo = tolower(ch);
-          // if (largo >= '0' && largo >= '9' || (largo >= 'a' && largo <= 'f')){
-          // b = strtol(largo, NULL, 16);
-          // map[r*16+c]=b;
+//   do {
+//     ch = leeChar();
+//     switch (ch) {
+//     case 0x1b5b41:
+//       r = (r > 0) ? r - 1 : 24;
+//       break;
+//     case 0x1b5b42:
+//       r = (r < 24) ? r + 1 : 0;
+//       break;
+//     case 0x1b5b43:
+//       c = (c < 31) ? c + 1 : 0;
+//       break;
+//     case 0x1b5b44:
+//       c = (c > 0) ? c - 1 : 31;
+//       break;
+//     default:
+//       if (c < 16) {
+//         char largo[3];
+//         char o = tolower(ch);
+//         ch2 = leeChar();
+//         char m = tolower(ch2);
+//         char * fin = "\0";
+//         largo[0] = o;
+//         largo[1] = m;
+//         largo[2] = * fin;
+//         /* Validamos los dos caracteres ingresados*/
+//         /*int validaChars[2] = {0,0};
+//         if (o >= '0' && o >= '9' || (o >= 'a' && o <= 'f')){
+//           validaChars[0] =1;
+//         }
+//         if (o >= '0' && o >= '9' || (o >= 'a' && o <= 'f')){
+//           validaChars[1] =1;
+//         }
+//         if(validaChars[0] == 1 && validaChars[1] == 1){
+//           b = strtol(largo, NULL, 16);
+//           map[r * 16 + c] = b;
+//         }*/
+//         b = strtol(largo, NULL, 16);
+//          map[r * 16 + c] = b;
+//       }
+//       if (c > 16) {
+//         char n = tolower(ch);
+//         if (n >= '0' && n <= '9' || (n >= 'a' && n <= 'f')) {
+//           // b = strtol(n, NULL, 16);
+//           map[r * 16 + c - 16] = n;
+//           // char largo = tolower(ch);
+//           // if (largo >= '0' && largo >= '9' || (largo >= 'a' && largo <= 'f')){
+//           // b = strtol(largo, NULL, 16);
+//           // map[r*16+c]=b;
 
-          // printf(" %c",ch);
-          // jovas[0] = tolower(ch);
-          //   jovas[1] = '\0';
-          //   char largo = tolower(jovas);
-          //   if (largo >= '0' && largo <= '9' || (largo >= 'a' && largo <= 'f')){
-          //   b = strtol(largo,NULL,16);
-          //   map[r*16+c]=b;
+//           // printf(" %c",ch);
+//           // jovas[0] = tolower(ch);
+//           //   jovas[1] = '\0';
+//           //   char largo = tolower(jovas);
+//           //   if (largo >= '0' && largo <= '9' || (largo >= 'a' && largo <= 'f')){
+//           //   b = strtol(largo,NULL,16);
+//           //   map[r*16+c]=b;
 
-          // char largo = tolower(ch);
-          // if (largo >= '0' && largo <= '9' || (largo >= 'a' && largo <= 'f')){
-          // b = strtol(largo, NULL, 16);
-          // map[r*16+c]=b;
-        }
-      } else {
-        if (isprint(ch)) {
-          map[r * 16 + c - 16] = ch;
+//           // char largo = tolower(ch);
+//           // if (largo >= '0' && largo <= '9' || (largo >= 'a' && largo <= 'f')){
+//           // b = strtol(largo, NULL, 16);
+//           // map[r*16+c]=b;
+//         }
+//       } else {
+//         if (isprint(ch)) {
+//           map[r * 16 + c - 16] = ch;
 
-          char * l = hazLinea(map, r * 16);
-          mvprintw(r, 0, l);
-        }
-        break;
-      }
-    }
+//           char * l = hazLinea(map, r * 16);
+//           mvprintw(r, 0, l);
+//         }
+//         break;
+//       }
+//     }
 
-    y = r;
-    x = (c < 16) ? c * 3 + 9 : 40 + c;
+//     y = r;
+//     x = (c < 16) ? c * 3 + 9 : 40 + c;
 
-    move(26, 5);
-    printw("Estoy en %d,%d: Lei %0x    ", r, c, ch);
-    move(y, x);
+//     move(26, 5);
+//     printw("Estoy en %d,%d: Lei %0x    ", r, c, ch);
+//     move(y, x);
 
-    refresh();
-  } while (ch != 24);
+//     refresh();
+//   } while (ch != 24);
 
-  endwin();
-  /*Libera el map, fs tan grande como quedo el archivo es el archivo de arriba*/
-  if (munmap(map, fs) == -1) {
-    perror("Error");
-  }
-  if (munmap(mapo, fs) == -1) {
-    perror("Error");
-  }
-  close(fde);
-  close(fdl);
-  return 0;
-}
+//   endwin();
+//   /*Libera el map, fs tan grande como quedo el archivo es el archivo de arriba*/
+//   if (munmap(map, fs) == -1) {
+//     perror("Error");
+//   }
+//   if (munmap(mapo, fs) == -1) {
+//     perror("Error");
+//   }
+//   close(fde);
+//   close(fdl);
+//   return 0;
+// }
 
 
 
